35a4f5dbabb57fb941199b85e4e6a32681e2bdad
tvarettoni <tvarettoni@itba.edu.ar>
2025-12-07 18:50:27 -0300

    started request parser

--- src/server/include/request.h
+++ src/server/include/request.h
@@ -2,11 +2,34 @@
 #define _REQUEST_H_
 
 #include "../../lib/selector/selector.h"
+#include "../../lib/parser/parser.h"
+#define MAX_ADDR_SIZE 255
+#define PORT_SIZE 2
 
 void request_read_init(const unsigned state, struct selector_key *key);
 unsigned request_read(struct selector_key *key);
 void dns_lookup(const unsigned state, struct selector_key *key);
 void try_connect(const unsigned state, struct selector_key *key);
 unsigned request_write(struct selector_key *key);
+
+struct request_parser {
+    struct parser * p;
+    uint8_t atyp;
+    uint8_t cmd;
+    uint8_t addr_len;
+    uint8_t dst_port[PORT_SIZE];
+    uint8_t dst_addr[MAX_ADDR_SIZE];
+};
+
+typedef enum {
+  REQUEST_STATE_VER = 0,  // waiting for VER field
+  REQUEST_STATE_CMD,      // waiting for CMD field
+  REQUEST_STATE_RSV,      // waiting for RSV field
+  REQUEST_STATE_ATYP,     // waiting for ATYP field
+  REQUEST_STATE_DSTADDR,  // waiting for dst addr field
+  REQUEST_STATE_DSTPORT,  // waiting for dst port field
+  REQUEST_STATE_FIN,      // finished parsing request
+  REQUEST_STATE_ERROR     // error while parsing hello
+} request_state_t;
 
 #endif
--- src/server/request.c
+++ src/server/request.c
@@ -1,11 +1,46 @@
 #include "request.h"
 
-void request_read_init(const unsigned state, struct selector_key *key) {}
+static void act_ver(struct parser_event *ret, const uint8_t c) {
+  ret->type = REQUEST_STATE_VER;
+  ret->n = 1;
+  ret->data[0] = c;
+}
 
-unsigned request_read(struct selector_key *key) { return 0; }
+static void act_cmd(struct parser_event *ret, const uint8_t c) {
+  ret->type = REQUEST_STATE_CMD;
+  ret->n = 1;
+  ret->data[0] = c;
+}
+
+static void act_atyp(struct parser_event *ret, const uint8_t c) {
+  ret->type = REQUEST_STATE_ATYP;
+  ret->n = 1;
+  ret->data[0] = c;
+}
+
+static struct parser_state_transition ST_VER[] = {
+    {ANY, REQUEST_STATE_CMD, act_ver, NULL}
+};
+
+static struct parser_state_transition ST_CMD[] = {
+    {ANY, REQUEST_STATE_RSV, act_cmd, NULL}
+};
+
+static struct parser_state_transition ST_RSV[] = {
+    {ANY, REQUEST_STATE_ATYP, NULL, NULL}
+};
+    
+static struct parser_state_transition ST_ATYP[] = {
+    {ANY, REQUEST_STATE_DSTADDR, act_atyp, NULL}
+};    
+
+
+    
+
+unsigned read_request(struct selector_key *key) { return 0; }
 
 void dns_lookup(const unsigned state, struct selector_key *key) {}
 
 void try_connect(const unsigned state, struct selector_key *key) {}
 
-unsigned request_write(struct selector_key *key) { return 0; }
+unsigned write_request(struct selector_key *key) { return 0; }
