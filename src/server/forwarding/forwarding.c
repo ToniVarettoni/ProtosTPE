37972d38e10b92561803651fe55c4b1adbc7a488
Emilio Mitchell <emitchell@itba.edu.ar>
2025-12-06 21:10:57 -0300

    feat(users): implemented user_delete_function

--- src/server/forwarding/forwarding.c
+++ src/server/forwarding/forwarding.c
@@ -1,8 +1,8 @@
 #include "forwarding.h"
 
-unsigned write_forward(struct selector_key *key) {}
+unsigned write_forward(struct selector_key *key) { return 0; }
 
-unsigned read_forward(struct selector_key *key) {}
+unsigned read_forward(struct selector_key *key) { return 0; }
 
 void setup_forward(const unsigned state, struct selector_key *key) {}
 
